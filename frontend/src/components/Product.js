11b6503a49dc825da394ca45b26a4b07a66db8a6
Alegriaa <brpereir@ucsc.edu>
2021-07-24 15:21:16 -0700

    implemented the react router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 import Rating from "./Rating";
-import products from "../products";
 
 /*
 Product component that is currently used in the homescreen
@@ -13,15 +13,15 @@ const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       {/* backtick template literal */}
-      <a href={`/product/${product._id}`}>
+      <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as="div">
           <Rating
             value={product.rating}
