ff24f2eb3cc5d88b70267d56847a5eb96a1966e3
Alegriaa <brpereir@ucsc.edu>
2021-08-19 17:59:13 -0700

    implementing homescreen redux state

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -4,7 +4,7 @@ export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: "PRODUCT_LIST_REQUEST" });
 
-    const { data } = await axios.get("api/products");
+    const { data } = await axios.get(`api/products`);
 
     dispatch({
       type: "PRODUCT_LIST_SUCCESS",
@@ -13,6 +13,27 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: "PRODUCT_LIST_FAIL",
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: "PRODUCT_DETAILS_REQUEST" });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: "PRODUCT_DETAILS_SUCCESS",
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: "PRODUCT_DETAILS_FAIL",
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
