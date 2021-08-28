cd45603e7d850ff82307531fc6fa092c12f8e666
Alegriaa <brpereir@ucsc.edu>
2021-08-27 23:46:52 -0700

    removing items form cart implemented

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,11 +1,11 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants /cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants /cartConstants";
 
 export const addToCart = (id, quantity) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
 
   dispatch({
-    type: "CART_ADD_ITEM",
+    type: CART_ADD_ITEM,
     payload: {
       product: data._id,
       name: data.name,
@@ -14,6 +14,14 @@ export const addToCart = (id, quantity) => async (dispatch, getState) => {
       countInStock: data.countInStock,
       quantity,
     },
+  });
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
   });
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
